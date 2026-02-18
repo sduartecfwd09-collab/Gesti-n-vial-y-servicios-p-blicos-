82260178c5159c6edc32018fb4c739fb16471d4d
SaraíDC <sduartecfwd09@gmail.com>
2026-02-18 11:40:40 -0600

    actualización sin permiso

--- public/js/administrador.js
+++ public/js/administrador.js
@@ -1,4 +1,9 @@
-let btnCerrarSeccion = document.querySelector(".btnCerrarSeccion")
+import { getUsuarios } from "../services/serviceUsuarios.js"
+import { updatePatchUsuarios } from "../services/serviceUsuarios.js"
+import { deleteUsuarios } from "../services/serviceUsuarios.js"
+
+
+
 
 const usuarioActivo = JSON.parse(localStorage.getItem("usuarioLogueado"))
 
@@ -52,4 +57,110 @@ if (usuarioActivo) {
         window.location.href = "index.html"
     })
     navCerrar.appendChild(btnCerrar);
+}
+
+
+
+
+
+///////////Funciones del trabajo extra///////////
+
+let contenedorRoles = document.getElementById("contenedorRoles")
+
+const usuariosActivo = JSON.parse(localStorage.getItem("usuarioLogueado"))
+
+const navCerrar2 = document.getElementById("navCerrar2"); /* nav dinamicos para varias condiciones, es lo mismo que hice en edit en la otro archivo al usar span */
+const navRoles = document.getElementById("navRoles");
+
+mostrarRoles();
+
+async function mostrarRoles() {
+    const roles = await getUsuarios();
+    contenedorRoles.textContent = "";
+
+    for (let i = 0; i < roles.length; i++) {
+        const rol = roles[i];
+
+        const fila = document.createElement("tr");
+
+        const tdnombre = document.createElement("td");
+        const tdrolActual = document.createElement("td");
+        const tdAcciones = document.createElement("td");
+
+
+        tdnombre.textContent = rol.nombre;
+        tdrolActual.textContent = rol.rol;
+        
+        
+
+        const btnEliminar = document.createElement("button");
+        const btnUsuario = document.createElement("button");
+        const btnAdministrador = document.createElement("button");;
+
+
+        btnEliminar.textContent = "Eliminar Usuario";
+        btnUsuario.textContent = "Usuario";
+        btnAdministrador.textContent = "Administrador";
+
+        
+         btnUsuario.addEventListener("click", async function () {
+            await updatePatchUsuarios(rol.id, { rol: "Usuario" });
+            mostrarRoles(); /* muestreme el cambio de inmediato despues de actualizar*/
+        });
+
+          btnAdministrador.addEventListener("click", async function () {
+            await updatePatchUsuarios(rol.id, { rol: "Administrador" });
+            mostrarRoles(); /* muestreme el cambio de inmediato despues de actualizar*/
+        });
+
+        btnEliminar.addEventListener("click", async function () {
+            await deleteUsuarios(rol.id);
+            mostrarRoles();
+        });
+
+
+
+        tdAcciones.appendChild(btnEliminar);
+        tdAcciones.appendChild(btnUsuario);
+        tdAcciones.appendChild(btnAdministrador);
+
+        fila.appendChild(tdnombre);
+        fila.appendChild(tdrolActual);
+         fila.appendChild(tdAcciones);
+       
+        contenedorRoles.appendChild(fila);
+
+    }
+}
+
+/* Parte del codigo dedicado a las nva dinamicas */
+if (usuarioActivo) {
+
+    const link = document.createElement("a");
+
+    if (usuarioActivo.rol === "usuario") {
+        link.textContent = "Panel Usuario";
+        link.href = "../pages/usuario.html";
+    }
+    else {
+        if (usuarioActivo.rol === "Administrador") {
+            link.textContent = "Administrador";
+            link.href = "../pages/administrador.html";
+        }
+    }
+
+
+    navRoles.appendChild(link);
+}
+
+/* a cerrar seccion, usando un boton que elimine el local storage */
+if (usuariosActivo) {
+
+    const btnCerrar = document.createElement("button");
+    btnCerrar.textContent = "Cerrar sesión";
+    btnCerrar.addEventListener("click", function () {
+        localStorage.removeItem("usuarioLogueado");
+        window.location.href = "index.html"
+    })
+    navCerrar2.appendChild(btnCerrar);
 }
